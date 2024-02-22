eebcf0c3287c859bb6d9305b01e1c5a1a6d37dfc
Matt <matthewho.2021@scis.smu.edu.sg>
2024-02-22 20:05:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import busio
 import board
 import boto3
 from decimal import Decimal
-import adafruit_amg88xx
 import RPi.GPIO as GPIO
 import math
 import mpu6050
@@ -12,11 +11,19 @@ import json
 # Frequency
 FREQ = 2
 
-# Set up thermal AMG8833
+# Set up ADS1115
 i2c = busio.I2C(board.SCL, board.SDA)
-amg = adafruit_amg88xx.AMG88XX(i2c)
+ads = ADS.ADS1115(i2c)
+channel = AnalogIn(ads, ADS.P0)
 
+def read_piezo():
+   return 32768 - channel.value
+
+# Set up MPU6050
 mpu6050 = mpu6050.mpu6050(0x68)
+
+def read_mpu6050():
+    return mpu6050.get_gyro_data()
 
 # Set up AWS
 file = open('keys.txt', 'r')
@@ -29,29 +36,19 @@ ddb = boto3.resource(
     aws_access_key_id=acki,
     aws_secret_access_key=asak
     )
-raw = ddb.Table("raw-data")
+raw = ddb.Table("toilet-data")
 
-def read_acc_temp():
-    gyroscope_data = mpu6050.get_gyro_data()
-    temperature = mpu6050.get_temp()
-
-    return gyroscope_data, temperature
-
+# Data Format
 class Record():
-    def __init__(self, therm, gyr, ambient_temp, has_motion, has_puddle=False):
+    def __init__(self, gyr, has_motion, pressure):
         self.timestamp = math.floor(time.time() * 1000)
-        self.therm = therm
         self.gyr = gyr
-        self.ambient_temp = ambient_temp
         self.has_motion = has_motion
-        self.has_puddle = has_puddle
+        self.pressure = pressure
 
 # Set up PIR
 GPIO.setmode(GPIO.BCM)
-LABEL_PIN = 26
 PIR_PIN = 27
-
-GPIO.setup(LABEL_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(PIR_PIN, GPIO.IN)
 
 print('Startup, give it 3 seconds')
@@ -59,8 +56,10 @@ time.sleep(3)
 print('READY')
 
 while True:
-    gyroscope, ambient = read_acc_temp()
-    rec = Record(amg.pixels, gyroscope, ambient, GPIO.input(PIR_PIN), GPIO.input(LABEL_PIN))
+    rec = Record(
+        read_mpu6050(), 
+        GPIO.input(PIR_PIN), 
+        read_piezo())
     
     item = json.loads(json.dumps(rec.__dict__), parse_float=Decimal)
     print(item)
