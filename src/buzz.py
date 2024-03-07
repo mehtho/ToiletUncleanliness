592aa05d5cc7beb6fa746199a103dae524b79df2
robpi2 <robpi2@rob.com>
2024-03-07 19:42:17 +0800

    buzzer gpio

--- src/buzz.py
+++ src/buzz.py
@@ -4,6 +4,7 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(17, GPIO.OUT)
 
 GPIO.output(17, GPIO.HIGH)
@@ -19,6 +20,10 @@ GPIO.add_event_detect(27, GPIO.BOTH, callback=callback)
 
 try:
 	while True:
-		time.sleep(.25)
+		time.sleep(2)
+		if GPIO.input(22):
+			print("Motion")
+
+
 except KeyboardInterrupt:
 	GPIO.cleanup()
