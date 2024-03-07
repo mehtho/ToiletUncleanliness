60902f50c41afcadce985df1628af9cda59c071d
Matt <matthewho.2021@scis.smu.edu.sg>
2024-03-07 21:02:22 +0800

    easy error handling

--- src/buzz.py
+++ src/buzz.py
@@ -1,29 +1,46 @@
 import RPi.GPIO as GPIO
 import time
+import requests
 
-GPIO.setmode(GPIO.BCM)
+stop = False
 
-GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(17, GPIO.OUT)
+fpurl = "https://g7p6olvo4b.execute-api.ap-southeast-1.amazonaws.com/prod/foot-pedal"
 
-GPIO.output(17, GPIO.HIGH)
+def run():
+	try:
+		GPIO.setmode(GPIO.BCM)
 
-def callback(channel):
-	if GPIO.input(27):
-		GPIO.output(17, GPIO.LOW)
-		time.sleep(.25)
-	else:
+		GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		GPIO.setup(17, GPIO.OUT)
+
 		GPIO.output(17, GPIO.HIGH)
+
+		beeping = False
+		def callback(channel):
+			if GPIO.input(27) and not beeping:
+				beeping = True
+				requests.post(fpurl, json={})
+				GPIO.output(17, GPIO.LOW)
+			else:
+				beeping = False
+				GPIO.output(17, GPIO.HIGH)
+				
+		GPIO.add_event_detect(27, GPIO.BOTH, callback=callback)
+
 		
-GPIO.add_event_detect(27, GPIO.BOTH, callback=callback)
-
-try:
-	while True:
-		time.sleep(2)
-		if GPIO.input(22):
-			print("Motion")
+		while True:
+			time.sleep(2)
+			if GPIO.input(22):
+				print("Motion")
+				pass
 
 
-except KeyboardInterrupt:
-	GPIO.cleanup()
+	except KeyboardInterrupt:
+		stop = True
+		GPIO.cleanup()
+	except Exception as e:
+		print("An error occurred:", e)
+
+while not stop:
+	run()
--- src/main.py
+++ src/main.py
@@ -13,58 +13,72 @@ import json
 # Frequency
 FREQ = 2
 
-# Set up ADS1115
-i2c = busio.I2C(board.SCL, board.SDA)
-ads = ADS.ADS1115(i2c)
-channel = AnalogIn(ads, ADS.P0)
+stop = False
 
-def read_piezo():
-   return 32768 - channel.value
+def run():
+	try:
+		# Set up ADS1115
+		i2c = busio.I2C(board.SCL, board.SDA)
+		ads = ADS.ADS1115(i2c)
+		channel = AnalogIn(ads, ADS.P0)
 
-# Set up MPU6050
-mpu6050 = mpu6050.mpu6050(0x68)
+		def read_piezo():
+			return 32767 - channel.value
 
-def read_mpu6050():
-    return mpu6050.get_accel_data()
+		# Set up MPU6050
+		mpu6050 = mpu6050.mpu6050(0x68)
 
-# Set up AWS
-file = open('keys.txt', 'r')
-acki = file.readline().strip()
-asak = file.readline().strip()
+		def read_mpu6050():
+			return mpu6050.get_accel_data()
 
-ddb = boto3.resource(
-    "dynamodb",
-    region_name="ap-southeast-1",
-    aws_access_key_id=acki,
-    aws_secret_access_key=asak
-    )
-raw = ddb.Table("toilet-data")
+		# Set up AWS
+		file = open('keys.txt', 'r')
+		acki = file.readline().strip()
+		asak = file.readline().strip()
 
-# Data Format
-class Record():
-    def __init__(self, gyr, has_motion, pressure):
-        self.timestamp = math.floor(time.time() * 1000)
-        self.gyr = gyr
-        self.has_motion = has_motion
-        self.pressure = pressure
+		ddb = boto3.resource(
+			"dynamodb",
+			region_name="ap-southeast-1",
+			aws_access_key_id=acki,
+			aws_secret_access_key=asak
+			)
+		raw = ddb.Table("toilet-data")
 
-# Set up PIR
-GPIO.setmode(GPIO.BCM)
-PIR_PIN = 23
-GPIO.setup(PIR_PIN, GPIO.IN)
+		# Data Format
+		class Record():
+			def __init__(self, gyr, has_motion, pressure):
+				self.timestamp = math.floor(time.time() * 1000)
+				self.gyr = gyr
+				self.has_motion = has_motion
+				self.pressure = pressure
 
-print('Startup, give it 3 seconds')
-time.sleep(3)
-print('READY')
+		# Set up PIR
+		GPIO.setmode(GPIO.BCM)
+		PIR_PIN = 23
+		GPIO.setup(PIR_PIN, GPIO.IN)
 
-while True:
-    rec = Record(
-        read_mpu6050(), 
-        GPIO.input(PIR_PIN), 
-        read_piezo())
-    
-    item = json.loads(json.dumps(rec.__dict__), parse_float=Decimal)
-    print(item)
-    raw.put_item(Item=item)
-    
-    time.sleep(FREQ)
+		print('Startup, give it 3 seconds')
+		time.sleep(3)
+		print('READY')
+
+		while True:
+			rec = Record(
+				read_mpu6050(), 
+				GPIO.input(PIR_PIN), 
+				read_piezo())
+			
+			item = json.loads(json.dumps(rec.__dict__), parse_float=Decimal)
+			print(item)
+			raw.put_item(Item=item)
+			
+			time.sleep(FREQ)
+
+	except KeyboardInterrupt:
+		stop = True
+		GPIO.cleanup()
+	except Exception as e:
+		print("An error occurred:", e)
+	
+
+while not stop:
+	run()
